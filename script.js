d2213174c0bad4d86f45bf82c4104744616db266
leomark92 <markobogush@gmail.com>
2022-07-14 18:48:26 +0300

    finalFixes2

--- script.js
+++ script.js
@@ -57,8 +57,6 @@ const computerPlay = () => {
 //after the start button is clicked the round starts when user clicks one of the choise buttons
 //after the user clicks one of the rock/paper/scissors button the PC automatically makes a choise
 //the round is finished when one of the players user or the pc reaches to 5 points
-
-
 let pcPoints = 0;
 let userPoints = 0;
 const playerPlay = () => {
@@ -66,8 +64,6 @@ let roundCounter = 0;
     
     
 selectBtn.addEventListener('click', function rpsSelection(e)  {
-        
-    
     //the code below is wrapped in if to avoid the parent element of the 3 buttons to be accessible
         if (!e.target.classList.contains('bottom_cont')) {
             //update the round counter
@@ -75,22 +71,18 @@ selectBtn.addEventListener('click', function rpsSelection(e)  {
             //selecting the rock/paper/scissors depending on the button clicked
             if (e.target.classList.contains('rock')) {
                 playerGuess = 'rock';
-    
             } 
             if (e.target.classList.contains('paper')) {
                 playerGuess = 'paper';
-                
             } 
             if (e.target.classList.contains('scissors')) {
                 playerGuess = 'scissors';
-                
             } 
             showRound.textContent = `round: ${roundCounter}`;
         //inserting the relevant icon to the user choise window
         userSel.textContent = e.target.textContent;
         //assigning the computer play function to a variable to make it accesible
         let pcGuess2 = computerPlay();
-        
         
         //updating the score of PC or user depending on the winning combination
         //also giving a green color to the winning user/pc icon for a moment using settimeout
@@ -146,12 +138,9 @@ selectBtn.addEventListener('click', function rpsSelection(e)  {
                 userIcon.classList.remove('draw_color');
             }, 100);
         }
-        
         //showing the current round score for the user and pc
         playerScore.textContent = userPoints;
         pcScore.textContent = pcPoints;
-    
-       
        //after 5 rounds played removing the event listener from the buttons
        //after 5 rounds are played show the popup window calling the winner
        if(roundCounter >= 5) {
@@ -173,7 +162,6 @@ selectBtn.addEventListener('click', function rpsSelection(e)  {
             }
             startText.classList.remove('popup_win_show');
         }
-        
         //Clicking the start button will reset the score to 0 for both players
         startBtn.addEventListener('click', () => {
             //also remove the event listener with the function when start button clicked
@@ -190,19 +178,16 @@ selectBtn.addEventListener('click', function rpsSelection(e)  {
             userIcon.classList.remove('winning_color');
             pcIcon.classList.remove('draw_color');
             userIcon.classList.remove('draw_color');
-
         });
     });    
 };
 
 //start button will make the buttons to trigger the function to start the round
 startBtn.addEventListener('click', playerPlay,);
-
 //show the text above the start button indicating what to do to start the round
 startBtn.addEventListener('click', () => {
     startText.classList.add('popup_win_show');
 });
-
 //closing the popup window
 popupBtn.addEventListener('click', () => {
     popupWin.classList.remove('popup_win_show');
